1367e6b2ce7679cc90b3d47c38d25d04241a54de
Malikov <malikov.b@yandex.ru>
2024-03-04 10:14:04 +0700

    1

--- block_users.py
+++ block_users.py
@@ -6,13 +6,13 @@ from log_init import logger
 with requests.Session() as session:
     users = session.get(f'https://{url}/api/v4/users', headers={'PRIVATE-TOKEN': token}, verify=False)
     for user in users.json():
-        if not user['is_admin']:
+        if not user["is_admin"]:
             block = session.post(f'https://{url}/api/v4/users/{user["id"]}/block',
                                  headers={'PRIVATE-TOKEN': token},
                                  verify=False)
-            logger.debug(f'{user['username']} - {block.json()}')
+            logger.debug(f'{user["username"]} - {block.json()}')
         else:
-            logger.debug(f'{user['username']} - admin')
+            logger.debug(f'{user["username"]} - admin')
 
 
 
